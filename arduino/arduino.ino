ac3b049169f5052c99e4d29d02ba4f90ca8e694c
알파 <arpa1705@gmail.com>
2024-12-16 21:29:59 +0900

    update

---
+++ arduino/arduino.ino
@@ -0,0 +1,151 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+#include <DHT.h>
+
+// 소프트웨어 시리얼 핀 설정
+#define RX_PIN 5  // Arduino의 디지털 핀 5
+#define TX_PIN 6  // Arduino의 디지털 핀 6
+#define SERVO_PIN A5
+#define DC_PIN_A1 9
+#define DC_PIN_A2 10
+#define DHT_PIN A0
+#define DUST_PIN A1
+#define WATER_PIN A3
+#define LED_PIN 3
+
+// DHT 온습도 센서 설정
+DHT dht(DHT_PIN, DHT11);
+
+float dustTmp = 0;
+
+Servo servo;
+SoftwareSerial espSerial(RX_PIN, TX_PIN);  // RX, TX 핀 설정
+
+void splitString(String data, char delimiter, String result[], int &count) {
+  int startIndex = 0;
+  int delimiterIndex = 0;
+  count = 0;  // 결과 배열에 저장된 파트 수 초기화
+
+  // delimiter가 더 이상 없을 때까지 반복
+  while ((delimiterIndex = data.indexOf(delimiter, startIndex)) != -1) {
+    result[count++] = data.substring(startIndex, delimiterIndex);
+    startIndex = delimiterIndex + 1;
+
+    // 결과 배열이 꽉 찼으면 멈춤 (안전 장치)
+    if (count >= 10) break;
+  }
+  // 마지막 부분 추가
+  result[count] = data.substring(startIndex);
+  result[count].trim();
+  count++;
+}
+
+// 미세먼지 데이터를 읽어오는 함수
+float read_dust() {
+  delayMicroseconds(280);
+  int voValue = analogRead(DUST_PIN);
+
+  float voltage = voValue * (5.0 / 1024.0);
+
+  float dustDensity = (voltage - 0.1) / 0.005;  // 미세먼지 농도 계산 (mg/m^3)
+  if (dustDensity < 0) dustDensity = 0;
+
+  if (dustDensity <= 1) return dustTmp;
+
+  float molecularWeight = 18.0;  // PM2.5의 평균 분자량
+  float ppm = (dustDensity * 24.45) / (molecularWeight);
+
+  dustTmp = ppm;
+
+  return ppm;
+}
+
+void setup() {
+  Serial.begin(4800);
+  espSerial.begin(4800);
+
+  servo.attach(SERVO_PIN);
+  delay(100);
+  servo.write(0);
+  servo.detach();
+  
+
+  dht.begin();
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(DC_PIN_A1, OUTPUT);
+  pinMode(DC_PIN_A2, OUTPUT);
+  digitalWrite(DC_PIN_A1, LOW);
+  digitalWrite(DC_PIN_A2, LOW);
+
+  Serial.println("Arduino Ready");
+}
+
+void loop() {
+  if (espSerial.available()) {
+    String data = espSerial.readStringUntil('\n');  // ESP8266으로부터 데이터 읽기
+    Serial.println("Received from ESP8266: " + data);
+
+    String parts[10];
+    int partCount = 0;
+
+    splitString(data, ':', parts, partCount);
+
+    String cmd = parts[0];
+
+    if (cmd == "DC") {
+      if (parts[1] == "1") {
+        Serial.println("dc on");
+        digitalWrite(DC_PIN_A2, HIGH);
+      } else if (parts[1] == "0") {
+        Serial.println("dc off");
+        digitalWrite(DC_PIN_A2, LOW);
+      }
+    }
+    if (cmd == "LED") {
+      if (parts[1] == "1") {
+        Serial.println("led on");
+        digitalWrite(LED_PIN, HIGH);
+      } else if (parts[1] == "0") {
+        Serial.println("led off");
+        digitalWrite(LED_PIN, LOW);
+      }
+    }
+    if (cmd == "SERVO") {
+      if (parts[1] == "1") {
+        Serial.println("servo on");
+        servo.attach(SERVO_PIN);
+        delay(100);
+        servo.write(60);
+        delay(100);
+        servo.detach();
+      } else if (parts[1] == "0") {
+        Serial.println("servo off");
+        servo.attach(SERVO_PIN);
+        delay(100);
+        servo.write(0);
+        delay(100);
+        servo.detach();
+      }
+    }
+  }
+
+  // 미세먼지 데이터 읽기
+  float dust = read_dust();
+
+  // 온도 및 습도 읽기
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  int waterLevel = analogRead(WATER_PIN);
+
+  espSerial.print("TEMP:");
+  espSerial.println(temperature);
+  espSerial.print("HUMI:");
+  espSerial.println(humidity);
+  espSerial.print("WATER:");
+  espSerial.println(waterLevel);
+  espSerial.print("DUST:");
+  espSerial.println(dust);
+
+  delay(1000);
+}
---
+++ esp8266/esp8266.ino
@@ -0,0 +1,174 @@
+#include <ESP8266WiFi.h>
+#include <SocketIoClient.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+#include <TaskScheduler.h>
+
+// 소프트웨어 시리얼 핀 설정
+#define RX_PIN 14  // NodeMCU의 D5 핀
+#define TX_PIN 12  // NodeMCU의 D6 핀
+
+Scheduler scheduler;
+
+SoftwareSerial arduinoSerial(RX_PIN, TX_PIN);
+
+const char* ssid = "황부연의 iPhone";
+const char* password = "gamemode1!";
+const char* host = "172.20.10.4";
+const int port = 8080;
+
+float temperature = 0;
+float humidity = 0;
+int waterLevel = 0;
+float dust = 0;
+
+bool securityMode = false;
+bool windowOpen = false;
+bool fanOn = false;
+bool lightOn = false;
+
+StaticJsonDocument<256> jsonDoc;
+
+SocketIoClient socketIO;
+
+void splitString(String data, char delimiter, String result[], int& count) {
+  int startIndex = 0;
+  int delimiterIndex = 0;
+  count = 0;  // 결과 배열에 저장된 파트 수 초기화
+
+  // delimiter가 더 이상 없을 때까지 반복
+  while ((delimiterIndex = data.indexOf(delimiter, startIndex)) != -1) {
+    result[count++] = data.substring(startIndex, delimiterIndex);
+    startIndex = delimiterIndex + 1;
+
+    // 결과 배열이 꽉 찼으면 멈춤 (안전 장치)
+    if (count >= 10) break;
+  }
+  // 마지막 부분 추가
+  result[count] = data.substring(startIndex);
+  result[count].trim();
+  count++;
+}
+
+void messageEventHandler(const char* payload, size_t length) {
+  Serial.printf("Message received: %s\n", payload);
+}
+
+void actionUpdatedEventHandler(const char* payload, size_t length) {
+  Serial.println("aaaaaaaaaaaaaaaaaaaaaaaa");
+  StaticJsonDocument<256> jsonDoc;
+
+  // JSON 문자열을 파싱
+  DeserializationError error = deserializeJson(jsonDoc, payload);
+
+  // 파싱 오류 처리
+  if (error) {
+    Serial.print("JSON Parsing failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  // JSON 데이터 읽기
+  bool newSecurityMode = jsonDoc["securityMode"];
+  bool newWindowOpen = jsonDoc["windowOpen"];
+  bool newFanOn = jsonDoc["fanOn"];
+  bool newLightOn = jsonDoc["lightOn"];
+
+  if (windowOpen != newWindowOpen) {
+    if (newWindowOpen) {
+      arduinoSerial.println("SERVO:1");
+    } else {
+      arduinoSerial.println("SERVO:0");
+    }
+  }
+
+  if (fanOn != newFanOn) {
+    if (newFanOn) {
+      arduinoSerial.println("DC:1");
+    } else {
+      arduinoSerial.println("DC:0");
+    }
+  }
+
+  if (lightOn != newLightOn) {
+    if (newLightOn) {
+      Serial.println("led = ");
+      Serial.println(lightOn);
+      arduinoSerial.println("LED:1");
+    } else {
+      arduinoSerial.println("LED:0");
+    }
+  }
+
+  securityMode = newSecurityMode;
+  windowOpen = newWindowOpen;
+  fanOn = newFanOn;
+  lightOn = newLightOn;
+}
+
+Task socketTask(50, TASK_FOREVER, &socketTaskCallback);
+Task serialTask(1000, TASK_FOREVER, &serialTaskCallback);
+
+void setup() {
+  Serial.begin(4800);
+  arduinoSerial.begin(4800);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  socketIO.begin(host, port, "/socket.io/?transport=websocket");
+  socketIO.on("actionUpdated", actionUpdatedEventHandler);
+
+  scheduler.addTask(socketTask);
+  scheduler.addTask(serialTask);
+  socketTask.enable();
+  serialTask.enable();
+}
+
+void socketTaskCallback() {
+  socketIO.loop();
+}
+
+void serialTaskCallback() {
+  if (arduinoSerial.available()) {
+    String data = arduinoSerial.readStringUntil('\n');  // ESP8266으로부터 데이터 읽기
+    Serial.println("Received from Arduino: " + data);
+
+    String parts[10];
+    int partCount = 0;
+
+    splitString(data, ':', parts, partCount);
+
+    String cmd = parts[0];
+
+    if (cmd == "TEMP") {
+      temperature = parts[1].toFloat();
+      jsonDoc["temperature"] = temperature;
+    }
+    if (cmd == "HUMI") {
+      humidity = parts[1].toFloat();
+      jsonDoc["humidity"] = humidity;
+    }
+    if (cmd == "WATER") {
+      waterLevel = parts[1].toInt();
+      jsonDoc["waterLevel"] = waterLevel;
+    }
+    if (cmd == "DUST") {
+      dust = parts[1].toFloat();
+      jsonDoc["dust"] = dust;
+    }
+
+    String jsonString;
+    serializeJson(jsonDoc, jsonString);
+
+    socketIO.emit("updateSensorMeasurements", jsonString.c_str());
+  }
+}
+
+void loop() {
+  scheduler.execute();
+}
